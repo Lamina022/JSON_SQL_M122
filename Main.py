e9b449d288374f9f8cb0997cd7dfaa80addbb8a4
Lisa Wuethrich <lamina.anju@outlook.com>
2020-09-11 17:19:13 +0200

    itn

--- Main.py
+++ Main.py
@@ -4,26 +4,26 @@
 import json
 
 # writing to JSON file   
-json_data = '[{"Employee ID":1,"Name":"Abhishek","Designation":"Software Engineer"},' \
-            '{"Employee ID":2,"Name":"Garima","Designation":"Email Marketing Specialist"}]'
+json_data = '{"people":[{"Employee ID":1,"Name":"Abhishek","Designation":"Software Engineer"},' \
+            '{"Employee ID":2,"Name":"Garima","Designation":"Email Marketing Specialist"}]}'
    
 json_object = json.loads(json_data) 
    
 # Indent keyword while dumping the 
 # data decides to what level  
-# spaces the user wants. 
-print(json.dumps(json_object, indent = 1)) 
-   
-# Difference in the spaces  
-# near the brackets can be seen 
-print(json.dumps(json_object, indent = 3)) 
+# spaces the user wants.
+
+data = json.dumps(json_object, indent = 3)
+
+with open('data.txt', 'w') as outfile:
+    json.dump(json_data, outfile)
     
 
 # reading from JSON file
-#with open('data.txt') as json_file:
-#    data = json.load(json_file)
-#    for p in data['people']:
-#        print('Name: ' + p['name'])
-#        print('Website: ' + p['website'])
-#        print('From: ' + p['from'])
-#        print('')
+with open('data.txt') as json_file:
+    data = json.load(json_file)
+    for p in data['people']:
+        print('Employee ID' + p['employee_id'])
+        print('Name: ' + p['name'])
+        print('Designation: ' + p['designation'])
+        print('')
